cb1bac20da73e1b51d259b948b4b7ea446be0fc2
Keisuke Yamauchi <keisuke.yamauchi.321@gmail.com>
2024-02-18 20:55:40 +0900

    update: Francのequalsの中身をMoneyに置き換える

--- src/main/java/money/Franc.java
+++ src/main/java/money/Franc.java
@@ -9,8 +9,8 @@ public class Franc extends Money {
     }
 
     public boolean equals(Object object) {
-        Franc franc = (Franc) object;
-        return amount == franc.amount;
+        Money money = (Money) object;
+        return amount == money.amount;
     }
 }
 
