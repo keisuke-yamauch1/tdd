3a72a56af48cb7fe8a491bee77a7d772a96511e4
Keisuke Yamauchi <keisuke.yamauchi.321@gmail.com>
2024-02-18 21:16:50 +0900

    update: currencyを親に移動

--- src/main/java/money/Dollar.java
+++ src/main/java/money/Dollar.java
@@ -1,17 +1,12 @@
 package money;
 
 public class Dollar extends Money {
-    private String currency;
     public Dollar(int amount) {
         this.amount = amount;
         this.currency = "USD";
     }
     Money times(int multiplier) {
         return new Dollar(amount * multiplier);
-    }
-
-    String currency() {
-        return currency;
     }
 }
 
--- src/main/java/money/Franc.java
+++ src/main/java/money/Franc.java
@@ -1,17 +1,12 @@
 package money;
 
 public class Franc extends Money {
-    private String currency;
     public Franc(int amount) {
         this.amount = amount;
         this.currency = "CHF";
     }
     Money times(int multiplier) {
         return new Franc(amount * multiplier);
-    }
-
-    String currency() {
-        return currency;
     }
 }
 
