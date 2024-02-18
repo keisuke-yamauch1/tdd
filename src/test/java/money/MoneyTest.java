60aec0f7089ec13651b67662068c67ef88633042
Keisuke Yamauchi <keisuke.yamauchi.321@gmail.com>
2024-02-18 21:39:43 +0900

    update: 不要なテスト削除

--- src/test/java/money/MoneyTest.java
+++ src/test/java/money/MoneyTest.java
@@ -22,8 +22,6 @@ public class MoneyTest {
     public void testEquality() {
         assertTrue(Money.dollar(5).equals(Money.dollar(5)));
         assertFalse(Money.dollar(5). equals(Money.dollar(6)));
-        assertTrue(Money.franc(5).equals(Money.franc(5)));
-        assertFalse(Money.franc(5). equals(Money.franc(6)));
         assertFalse(Money.franc(5).equals(Money.dollar(5)));
     }
 
@@ -32,10 +30,5 @@ public class MoneyTest {
         Money five = Money.franc(5);
         assertEquals(Money.franc(10), five.times(2));
         assertEquals(Money.franc(15), five.times(3));
-    }
-
-    @Test
-    public void testDifferentClassEquality() {
-        assertTrue(new Money(10, "CHF").equals(new Franc(10, "CHF")));
     }
 }
