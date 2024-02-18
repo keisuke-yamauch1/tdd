e42209197a802f02942371339989ca03b084a4fa
Keisuke Yamauchi <keisuke.yamauchi.321@gmail.com>
2024-02-18 15:55:03 +0900

    update: amount *= multiplier

--- src/main/java/money/Dollar.java
+++ src/main/java/money/Dollar.java
@@ -3,9 +3,10 @@ package money;
 public class Dollar {
     int amount;
     public Dollar(int amount) {
+        this.amount = amount;
     }
     void times(int multiplier) {
-        amount = 5 * 2;
+        amount *= multiplier;
     }
 }
 
