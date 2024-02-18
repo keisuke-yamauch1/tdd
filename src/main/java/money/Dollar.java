d26f38d3abe8c28f5e11c4e410411c7606413b37
Keisuke Yamauchi <keisuke.yamauchi.321@gmail.com>
2024-02-18 21:15:49 +0900

    update: currencyをプロパティで持つ

--- src/main/java/money/Dollar.java
+++ src/main/java/money/Dollar.java
@@ -1,15 +1,17 @@
 package money;
 
 public class Dollar extends Money {
+    private String currency;
     public Dollar(int amount) {
         this.amount = amount;
+        this.currency = "USD";
     }
     Money times(int multiplier) {
         return new Dollar(amount * multiplier);
     }
 
     String currency() {
-        return "USD";
+        return currency;
     }
 }
 
