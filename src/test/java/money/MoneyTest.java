4928e61bd0fe90d58ec620a16db2cd9e2b45af42
Keisuke Yamauchi <keisuke.yamauchi.321@gmail.com>
2024-02-18 23:24:21 +0900

    update: プラスするテスト作成

--- src/test/java/money/MoneyTest.java
+++ src/test/java/money/MoneyTest.java
@@ -24,4 +24,10 @@ public class MoneyTest {
         assertFalse(Money.dollar(5). equals(Money.dollar(6)));
         assertFalse(Money.franc(5).equals(Money.dollar(5)));
     }
+
+    @Test
+    public void testSimpleAddition() {
+        Money sum = Money.dollar(5).plus(Money.dollar(5));
+        assertEquals(Money.dollar(10), sum);
+    }
 }
