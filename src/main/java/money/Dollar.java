f7a828e84122c5ba7f174e71d90adbb715010ccc
Keisuke Yamauchi <keisuke.yamauchi.321@gmail.com>
2024-02-18 20:28:06 +0900

    update: amountを比較に変更

--- src/main/java/money/Dollar.java
+++ src/main/java/money/Dollar.java
@@ -10,7 +10,8 @@ public class Dollar {
     }
 
     public boolean equals(Object object) {
-        return true;
+        Dollar dollar = (Dollar) object;
+        return amount == dollar.amount;
     }
 }
 
