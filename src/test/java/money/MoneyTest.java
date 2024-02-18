ea81db6ff43590acdcbf9c34fc3c5248cf575466
Keisuke Yamauchi <keisuke.yamauchi.321@gmail.com>
2024-02-18 15:44:01 +0900

    add: MoneyTest

---
+++ src/test/java/money/MoneyTest.java
@@ -0,0 +1,12 @@
+package money;
+
+import org.junit.Test;
+
+public class MoneyTest {
+    @Test
+    public void testMultiplication() {
+        Dollar five = new Dollar(5);
+        five.times(2);
+        assertEquals(10, five.amount);
+    }
+}
